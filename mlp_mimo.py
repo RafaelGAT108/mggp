53f04b55362f0716053b43591c54f6be7b435d2c
rafael <rafael@rafael>
2024-06-12 22:14:15 -0300

    initial development of mlp_mimo.py

---
+++ mlp_mimo.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from sklearn.model_selection import train_test_split
+
+
+def create_sequences(database, lag):
+    xs = np.array([database[i-lag:i, :].flatten() for i in range(lag, len(database))])
+    ys = np.array([database[i, 2:5] for i in range(lag, len(database))])
+
+    return xs, ys
+
+
+df_train = pd.read_csv("F16GVT_Files/BenchmarkData/F16Data_FullMSine_Level3.csv").to_numpy()
+database_train = df_train[:, :5]
+
+df_test = pd.read_csv("F16GVT_Files/BenchmarkData/F16Data_FullMSine_Level2_Validation.csv").to_numpy()
+database_test = df_test[:, :5]
+
+lag_length = 5
+X_train, y_train = create_sequences(database_train, lag_length)
+X_test, y_test = create_sequences(database_test, lag_length)
+
+print("Shape of X:", X_train.shape)
+print("Shape of y:", y_train.shape)
+
+model = Sequential([
+    Dense(32, activation='relu', input_shape=(5 * lag_length,)),
+    Dense(32, activation='relu'),
+    Dense(3, activation='linear')
+])
+
+model.compile(optimizer='adam', loss='mse')
+
+model.summary()
+
+history = model.fit(X_train, y_train, epochs=20, validation_data=(X_test, y_test))
+
+loss = model.evaluate(X_test, y_test)
+print(f"RMSE: {round(np.sqrt(loss), 6)}")
+
+# predictions = model.predict(X_test)
+# print("Predictions:", predictions)
