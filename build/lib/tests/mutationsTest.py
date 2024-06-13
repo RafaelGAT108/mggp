fb80df665811567627dcff3bf5f2abe2531d7e59
rafael <rafael@rafael>
2024-06-12 22:11:01 -0300

    mutationsTest refactored

--- build/lib/tests/mutationsTest.py
+++ build/lib/tests/mutationsTest.py
@@ -6,13 +6,13 @@ Created on Mon Apr 24 16:56:36 2023
 """
 
 from base import Element
-from mutations import MutGPOneTree,MutGPUniform,MutGPReplace
+from mutations import MutGPOneTree, MutGPUniform, MutGPReplace
 from copy import deepcopy
 
 """
 Test MISO mutations
 """
-element = Element(weights=(-1,), delays=[1,2], nVars=2, nTerms=5, maxHeight=3, mode='MISO')
+element = Element(weights=(-1,), delays=[1, 2], nVars=2, nTerms=5, maxHeight=3, mode='MISO')
 ind = element.buildRandomModel()
 
 mutation = MutGPOneTree(element)
@@ -30,8 +30,8 @@ assert True
 """
 Test MIMO mutations
 """
-element = Element(weights=(-1,),delays=[1,2,3],nVars=4,nTerms=5,nOutputs=2,maxHeight=5,mode='MIMO')
-element.renameArguments({'ARG0':'y1','ARG1':'y2','ARG2':'u1','ARG3':'u2'})
+element = Element(weights=(-1,), delays=[1, 2, 3], nVars=4, nTerms=5, nOutputs=2, maxHeight=5, mode='MIMO')
+element.renameArguments({'ARG0': 'y1', 'ARG1': 'y2', 'ARG2': 'u1', 'ARG3': 'u2'})
 ind = element.buildRandomModel()
 
 mutation = MutGPOneTree(element)
