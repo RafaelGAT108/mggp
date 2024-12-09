92a837282d2d6f14a3774dcdc907958a25fcbeb5
Rafael <rafael.santos0t5@gmail.com>
2024-12-09 17:12:44 -0300

    included "if __name__ == __main__" in main file

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import pandas as pd
 from mggp import MGGP
 
-df_train = pd.read_csv("F16GVT_Files/BenchmarkData/F16Data_FullMSine_Level3.csv").to_numpy()
-# u_train, y_train = (df_train[:7372, :2], df_train[:7372, 2:5])
-u_train, y_train = (df_train[:, :2], df_train[:, 2:5])
-df_val = pd.read_csv("F16GVT_Files/BenchmarkData/F16Data_FullMSine_Level2_Validation.csv").to_numpy()
-u_val, y_val = (df_val[:, :2], df_val[:, 2:5])
-print(len(u_train), len(y_train))
+if __name__ == '__main__':
+    df_train = pd.read_csv("F16GVT_Files/BenchmarkData/F16Data_FullMSine_Level3.csv").to_numpy()
+    # u_train, y_train = (df_train[:7372, :2], df_train[:7372, 2:5])
+    u_train, y_train = (df_train[:, :2], df_train[:, 2:5])
+    df_val = pd.read_csv("F16GVT_Files/BenchmarkData/F16Data_FullMSine_Level2_Validation.csv").to_numpy()
+    u_val, y_val = (df_val[:, :2], df_val[:, 2:5])
+    print(len(u_train), len(y_train))
 
-mggp = MGGP(inputs=u_train,
-            outputs=y_train,
-            validation=(u_val, y_val),
-            nDelays=[1, 2, 3, 5, 10, 15, 25, 50],
-            # nDelays=2,
-            generations=50,
-            evaluationMode="RMSE",
-            evaluationType="FreeRun")
+    mggp = MGGP(inputs=u_train,
+                outputs=y_train,
+                validation=(u_val, y_val),
+                nDelays=[1, 2, 3, 5, 10, 15, 25, 50],
+                # nDelays=2,
+                generations=50,
+                evaluationMode="RMSE",
+                evaluationType="FreeRun")
 
-mggp.run()
+    mggp.run()
