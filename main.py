af558a289324ec4c9a45098ca9dd8772d88ad8e7
Rafael <rafael.santos0t5@gmail.com>
2024-12-10 13:15:50 -0300

    adjustments in main file

--- main.py
+++ main.py
@@ -7,15 +7,16 @@ if __name__ == '__main__':
     u_train, y_train = (df_train[:, :2], df_train[:, 2:5])
     df_val = pd.read_csv("F16GVT_Files/BenchmarkData/F16Data_FullMSine_Level2_Validation.csv").to_numpy()
     u_val, y_val = (df_val[:, :2], df_val[:, 2:5])
-    print(len(u_train), len(y_train))
+    # print(len(u_train), len(y_train))
 
     mggp = MGGP(inputs=u_train,
                 outputs=y_train,
                 validation=(u_val, y_val),
                 nDelays=[1, 2, 3, 5, 10, 15, 25, 50],
                 # nDelays=2,
-                generations=50,
+                generations=10,
                 evaluationMode="RMSE",
-                evaluationType="FreeRun")
+                k=10,
+                evaluationType="MShooting")
 
     mggp.run()
